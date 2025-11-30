1fb6595698a88574f610b6c20e278cd1324cb082
jamilahmed2 <ja0935991@gmail.com>
2025-12-01 03:58:09 +0500

    hellow world

---
+++ app/welcome/welcome.tsx
@@ -0,0 +1,10 @@
+import logoDark from "./logo-dark.svg";
+import logoLight from "./logo-light.svg";
+
+export function Welcome() {
+  return (
+    <main >
+      <h1>Hello world</h1>
+    </main>
+  );
+}
